ba4610393131d8d0696c68f6b863f3878827b5e0
Daichi OBINATA <earth2001y@gmail.com>
2013-04-21 10:11:33 +0900

    テストスクリプトを satelite-orbit-analysis を引くように変更

--- go-test.sh
+++ go-test.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-g++ -Wall -g -o soa-test test/*.cpp src/*.cpp -DUNITTEST -lcppunit -I/usr/local/include/eigen3
+SOA=$PWD/../satelite-orbit-analysis
+OPT="-DUNITTEST"
+
+rm -f *.o ./soa-test
+
+g++ -Wall -c $SOA/src/*.cpp $OPT
+g++ -Wall -c test/*.cpp src/*.cpp $OPT -I/usr/local/include/eigen3 -I$SOA/src
+g++ -Wall -o soa-test *.o -lcppunit
 [ $? -eq 0 ] && ./soa-test
 
